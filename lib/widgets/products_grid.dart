7c66f31faa298af70d86971b8de0250eaa1450e2
laevad <aldave.tabian@ustp.edu.ph>
2021-05-25 20:20:53 +0800

    working with Providers and listeners

---
+++ lib/widgets/products_grid.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../provider/products.dart';
+import 'product_item.dart';
+
+class ProductsGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    final products = productsData.items;
+    return GridView.builder(
+      padding: const EdgeInsets.all(10),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10),
+      itemBuilder: (context, index) => ProductItem(
+        products[index].id,
+        products[index].title,
+        products[index].imageUrl,
+      ),
+      itemCount: products.length,
+    );
+  }
+}
