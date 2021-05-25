5307613c688673e90c9fe34da9ddb7be8a5df939
laevad <aldave.tabian@ustp.edu.ph>
2021-05-25 17:28:59 +0800

    Styling and Theming the app

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -8,21 +8,29 @@ class ProductItem extends StatelessWidget {
   ProductItem(this.id, this.title, this.imageUrl);
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Image.network(
-        imageUrl,
-        fit: BoxFit.cover,
-      ),
-      footer: GridTileBar(
-        backgroundColor: Colors.black54,
-        leading: IconButton(onPressed: () {}, icon: Icon(Icons.favorite)),
-        title: Text(
-          title,
-          textAlign: TextAlign.center,
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: Image.network(
+          imageUrl,
+          fit: BoxFit.cover,
         ),
-        trailing: IconButton(
-          onPressed: () {},
-          icon: Icon(Icons.shopping_cart),
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.favorite),
+            color: Theme.of(context).accentColor,
+          ),
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.shopping_cart),
+            color: Theme.of(context).accentColor,
+          ),
         ),
       ),
     );
