93f2b84d0f7fd43a458f426558d33f4ca66e2d17
laevad <aldave.tabian@ustp.edu.ph>
2021-05-25 14:05:11 +0800

    defining a data model

---
+++ lib/screens/product_overview_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ProductOverviewScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
